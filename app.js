3b236c6c704b582e21c7f9cdf5b93b504aa51194
Karanvir Sidana <f20180174@pilani.bits-pilani.ac.in>
2021-04-08 02:41:04 +0530

    version 1.1

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.post("/compose", function(req, res) {
 });
 
 // Activating the server port at local host 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT ||3000, function() {
   console.log("Server started on port 3000");
 });
